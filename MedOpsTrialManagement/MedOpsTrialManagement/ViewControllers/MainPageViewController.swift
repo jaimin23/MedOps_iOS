49aed113c4fabec9ae4f3e705783c9d8eb06c000
Jaimin Patel <pajaimin@sheridancollege.ca>
2018-04-23 08:49:45 -0400

    Added login view along with hard coded log in check to go to main view

---
+++ MedOpsTrialManagement/MedOpsTrialManagement/ViewControllers/MainPageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainPageViewController.swift
+//  MedOpsTrialManagement
+//
+//  Created by Jaimin Patel on 2018-04-23.
+//  Copyright © 2018 Jaimin Patel. All rights reserved.
+//
+
+import Foundation
