69aaa7b25cdc0709cf98e9eb1edc287546a1887b
Jaimin Patel <pajaimin@sheridancollege.ca>
2018-04-24 13:31:29 -0400

    Turns out last I did empty commit, Added login logic with alert for checking login

---
+++ MedOpsTrialManagement/MedOpsTrialManagement/New Group/TriallMainController.swift
@@ -0,0 +1,13 @@
+//
+//  TrailMainController.swift
+//  MedOpsTrialManagement
+//
+//  Created by Jaimin Patel on 2018-04-24.
+//  Copyright © 2018 Jaimin Patel. All rights reserved.
+//
+
+import UIKit
+
+class TrailMainController:UIViewController{
+    
+}
